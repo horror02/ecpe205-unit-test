ec35294a35cc2218930b7be0978bcb8ad599ccc7
Jericho <ferninjericho07@gmail.com>
2022-05-10 22:48:03 +0800

    git commit -m

--- src/com/ecpe205/Calculator.java
+++ src/com/ecpe205/Calculator.java
@@ -13,14 +13,59 @@ public class Calculator {
 
     //create a method that computes base x power of y
     //use parameterizedTest methodsource - 5 sets to test
+    public double pow( double x, double y ){
+        double pow = 1;
+        for(int i = 1; i<=y; i++){
+            pow = pow * x;
+        }
+        return pow;
+    }
 
     //create a method that computes for the factorial of a value
     //use parameterizedTest- 5 values to test
+    public double factorial(double value) {
+        int n;
+        if (value >= 0) {
+            n = 1;
+            for (int i = 1; i <= value; i++) {
+                n = n * i;
+            }
+            return n;
+        }
+    }
 
     //create a method that checks if a given string is a palindrome
     //use parameterizedTest - 5 values to test
+        public boolean Palindrome(String value) {
+            int i = 0, j = value.length() - 1;
 
-    //create a method that accepts an array of integer values, the method then sorts the value in ascending order
-    //** you may choose whatever sort data structure (bubble sort, shell sort,...)
-    //use parameterizedTest methodSource
-}
+            while (i < j) {
+
+                if (value.charAt(i) != value.charAt(j))
+                    return false;
+                i++;
+                j--;
+            }
+
+
+            //create a method that accepts an array of integer values, the method then sorts the value in ascending order
+            //** you may choose whatever sort data structure (bubble sort, shell sort,...)
+            //use parameterizedTest methodSource
+
+            public int[] Sort ( int[] arr){
+                int temp;
+
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = i + 1; j < arr.length; j++) {
+                        if (arr[i] > arr[j]) {
+                            temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                        }
+                    } return arr;
+          }
+
+            }
+
+
+        }
--- src/com/ecpe205/CalculatorTest.java
+++ src/com/ecpe205/CalculatorTest.java
@@ -71,4 +71,70 @@ class CalculatorTest {
                 Arguments.of(new int[]{7,2,8,3,4})
         );
     }
+
+    @Test
+    @MethodSource("PowerBase")
+    void shouldPower(double x, double y){
+        assertEquals(Math.pow(x,y), calc.pow(x,y));
+    }
+
+    static Stream<Arguments> Power(){
+        return Stream.of(
+                Arguments.of(1,3),
+                Arguments.of(4,6),
+                Arguments.of(3,0),
+                Arguments.of(4,2),
+                Arguments.of(5,3)
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("FactorialValue")
+    void shouldFactorialValue(int a, int b){
+        assertEquals(a, calc.factorial(b));
+    }
+
+    static Stream<Arguments> FactorialValues () {
+        return Stream.of(
+                Arguments.of(3, 0),
+                Arguments.of(2, 4),
+                Arguments.of(9, 3),
+                Arguments.of(25, 4),
+                Arguments.of(30, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("PalindromeValue")
+    void shouldPalindrome(Boolean a, String value){
+        assertEquals(a, calc.Palindrome(value));
+    }
+
+    static Stream<Arguments> PalindromeInputValues () {
+        return Stream.of(
+                Arguments.of(true,"dad"),
+                Arguments.of(true,"rivever"),
+                Arguments.of(false,"World"),
+                Arguments.of(false,"Hello"),
+                Arguments.of(true,"sis")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("Sorting")
+    void shouldPalindromeOne(int[] a, int[] b){
+        assertEquals(a, calc.Sort(b));
+    }
+
+    static Stream<Arguments> ArrayInputValues () {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3,4,5}, new int[]{2,1,3,5,4}),
+                Arguments.of(new int[]{0,1,2,3,8},new int[]{8,0,1,3,2}),
+                Arguments.of(new int[]{1,3,5,7,8},new int[]{5,3,8,7,1}),
+                Arguments.of(new int[]{10,13,18,19,20},new int[]{18,10,13,20,19}),
+                Arguments.of(new int[]{2,3,6,9,10},new int[]{9,2,10,6,3})
+        );
+    }
+
 }
